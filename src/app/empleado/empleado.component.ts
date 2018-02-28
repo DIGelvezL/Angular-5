058cb030c221c46468f163c0b25b7f3b9f0646e1
INTERGRUPO\Dgelvez <dgelvez@intergrupo.com>
2018-02-28 08:31:40 -0500

    no message

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmpleadoEntity } from './empleadoEntity';
 
 @Component({
 	selector: 'empleado-tag',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
 })
 
 export class EmpleadoComponent{
-	public titulo = 'Titulo del componente empleado';
+	public titulo = 'Componente Empleados:';
+	public empleado:EmpleadoEntity;
+	public trabajadores:Array<EmpleadoEntity>;
+	public trabajador_externo:boolean;
+	public color:string;
+
+	constructor(){
+		this.empleado = new EmpleadoEntity("Daniel Gélvez", 28, "Developer", true);
+		this.trabajadores = [
+			new EmpleadoEntity("Laura Gélvez", 26, "Medica", true),
+			new EmpleadoEntity("Vilma Leon", 58, "Asistente", false),
+			new EmpleadoEntity("David Gaitan", 18, "Estudiante", false),
+		];
+
+		this.trabajador_externo = true;
+		this.color = 'green';
+	}
+
+	ngOnInit(){
+		console.log(this.empleado);
+		console.log(this.trabajadores);
+	}
+
+	cambiarExterno(valor){
+		this.trabajador_externo = valor;
+	}
 }
---
+++ src/app/empleado/empleadoEntity.ts
@@ -0,0 +1,9 @@
+export class EmpleadoEntity{
+
+	constructor(
+		public nombre:string,
+		public edad:number,
+		public cargo:string,
+		public contratado:boolean
+	){}
+}
